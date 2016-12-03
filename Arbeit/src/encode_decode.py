475e7bfc598b2b8d83283cb6e1cdd9f06d25a178
Your Name <you@example.com>
2016-12-03 13:12:13 +0100

    Besseres beispielbild

--- Arbeit/src/encode_decode.py
+++ Arbeit/src/encode_decode.py
@@ -74,7 +74,7 @@ def decode(compressedMacroblocks, subsample=True):
 
 if __name__ == "__main__":
     # show original image
-    bild = scipy.misc.imread('./test_img/lena.jpg')
+    bild = scipy.misc.imread('./test_img/brook.jpg')
     # bild = scipy.misc.imread('./test_img/lena_small.jpg')
     # bild = example1()
     img = create_img(bild)
